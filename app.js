5c5c0e643a1006f562fdf04d98815a4738fdc4a4
Duc <duc@ducs-mbp.localdomain>
2020-05-13 17:50:24 +0200

    exit handling

--- app.js
+++ app.js
@@ -83,3 +83,15 @@ io.on("connection", function (socket) {
 http.listen(port, () => {
   console.log(`Server app listening at ${host}:${port}`);
 });
+
+//exit handling
+process.stdin.resume();
+function exitHandler(options, exitCode) {
+    if (exitCode || exitCode === 0) console.log(0);
+    if (options.exit) process.exit();
+}
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
+process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
