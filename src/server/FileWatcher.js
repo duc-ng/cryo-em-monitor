89ae77d9a5c34390e7a298f5d881728d6a30ac62
Duc <duc-thinh@hotmail.de>
2020-11-18 18:47:08 +0100

    test 8

--- src/server/FileWatcher.js
+++ src/server/FileWatcher.js
@@ -5,6 +5,7 @@ const Reader = require("./Reader");
 const Memory = require("./Memory");
 const Logger = require("./Logger");
 const fs = require("fs");
+const glob = require("glob");
 
 class FileWatcher {
   constructor(subfolder) {
@@ -15,7 +16,7 @@ class FileWatcher {
     this.readCount = 0;
     this.directory = path.join(
       config.app.rootDir,
-      subfolder,
+      "Titan1",
       "*",
       "*",
       "*.star"
@@ -29,50 +30,43 @@ class FileWatcher {
     });
 
     this.watcher
-      .on("add", (dirPath) => this.read(dirPath, subfolder))
-      .on("change", (dirPath) => this.read(dirPath, subfolder))
-      .on('ready', () => console.log('Initial scan complete.'))
-    // this.watcher = sane(this.directory, {
-    //   glob: ["**/*.star"],
-    // });
-    // this.watcher.on("ready", function () {
-    //   console.log("ready");
-    // });
-    // this.watcher.on("add", function (filepath, root, stat) {
-    //   console.log("file added", filepath);
+      .on("ready", () => console.log("Initial scan complete."))
+      .on("add", (dirPath) => this.read(dirPath, subfolder));
+
+    // glob(this.directory, function (er, files) {
+    //   console.log(files);
     // });
   }
 
   read = async (filePath, subfolder) => {
-    const dirPath = filePath.substring(0, filePath.lastIndexOf("/"));
-    const dataStar = path.join(dirPath, "data.star");
-    const timesStar = path.join(dirPath, "times.star");
-    const imagesStar = path.join(dirPath, "images.star");
-
+    // const dirPath = filePath.substring(0, filePath.lastIndexOf("/"));
+    // const dataStar = path.join(dirPath, "data.star");
+    // const timesStar = path.join(dirPath, "times.star");
+    // const imagesStar = path.join(dirPath, "images.star");
     this.watcher.unwatch(filePath);
     this.readCount++;
     console.log(this.readCount + ". read path");
-    try {
-      // await fs.promises.access(dataStar);
-      // await fs.promises.access(timesStar);
-      // await fs.promises.access(imagesStar);
-      // const files = await Promise.all([
-      //   this.reader.readStarFile(dataStar),
-      //   this.reader.readStarFile(timesStar),
-      //   this.reader.readStarFile(imagesStar),
-      // ]);
-      // const merge = { ...files[0], ...files[1] };
-      // const obj = { path: dirPath, data: merge, times: files[2] };
-      // if (Object.keys(merge).length !== 0) {
-      //   this.memory.add(obj, subfolder);
-      // }
-    } catch (error) {
-      this.errorCount++;
-      this.logger.log(
-        "error",
-        "(File reading) " + this.errorCount + ". " + error
-      );
-    }
+    // try {
+    //   // await fs.promises.access(dataStar);
+    //   // await fs.promises.access(timesStar);
+    //   // await fs.promises.access(imagesStar);
+    //   // const files = await Promise.all([
+    //   //   this.reader.readStarFile(dataStar),
+    //   //   this.reader.readStarFile(timesStar),
+    //   //   this.reader.readStarFile(imagesStar),
+    //   // ]);
+    //   // const merge = { ...files[0], ...files[1] };
+    //   // const obj = { path: dirPath, data: merge, times: files[2] };
+    //   // if (Object.keys(merge).length !== 0) {
+    //   //   this.memory.add(obj, subfolder);
+    //   // }
+    // } catch (error) {
+    //   this.errorCount++;
+    //   this.logger.log(
+    //     "error",
+    //     "(File reading) " + this.errorCount + ". " + error
+    //   );
+    // }
   };
 
   set memory(val) {
