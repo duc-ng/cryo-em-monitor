d7d18f03135e474838728cc0bbd981b35f7def86
Duc <duc-thinh@hotmail.de>
2020-11-30 22:10:12 +0100

    add usepolling

--- src/server/FileWatcher.js
+++ src/server/FileWatcher.js
@@ -22,6 +22,7 @@ class FileWatcher {
       ignored: /^\./,
       persistent: true,
       awaitWriteFinish: true,
+      usePolling: true,
     });
     this.watcher
       .on("add", (dirPath) => this.read(dirPath, subfolder))
